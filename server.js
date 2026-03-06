143a4152832f9510ed9602fdb9e815f7bd3bb382
happyhappybillu <happyhappybilluu@gmail.com>
2026-03-06 20:31:23 +0530

    mm commit

--- server.js
+++ server.js
@@ -12,15 +12,27 @@ const { initSocket } = require("./src/services/socket.service");
 
 const PORT = process.env.PORT || 5000;
 
+// Root route (health check)
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "success",
+    message: "VANCROX Backend Running 🚀",
+  });
+});
+
+// connect database
 connectDB();
 
+// start background services
 startCronJobs();
 startDepositWatcher();
 
 const server = http.createServer(app);
 
+// socket initialize
 initSocket(server);
 
+// start server
 server.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
